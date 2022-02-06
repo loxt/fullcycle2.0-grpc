56cf45f965f8e801381933f6c1111d0986cd34f7
loxt <emannuelloxt@hotmail.com>
2022-02-06 16:08:20 -0400

    feat(): create method ClientStream in server

--- services/user.go
+++ services/user.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/loxt/fullcycle2.0-grpc/pb"
+	"io"
+	"log"
 	"time"
 )
 
@@ -65,4 +67,28 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 	time.Sleep(time.Second * 3)
 
 	return nil
+}
+
+func (user *UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	var users []*pb.User
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetName())
+	}
 }
